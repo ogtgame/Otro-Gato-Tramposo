4699167bbf27138d1732e831383cc0aef7c143d7
ogtgame <mnu.bgiusti@gmail.com>
2026-04-11 00:08:17 -0300

    Update server.js

--- server.js
+++ server.js
@@ -997,7 +997,7 @@ io.on('connection', (socket) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 server.listen(PORT, () => {
   console.log(`O.G.T. server corriendo en puerto ${PORT}`);
 });
